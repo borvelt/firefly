7ab66c7373f0bb257a9c2b5f0f457345af9882a1
borvelt <borvelt@borvelts-MacBook-Pro.local>
2016-04-09 16:59:35 +0430

    add report permissions

--- app/seeds/Authorization.php
+++ app/seeds/Authorization.php
@@ -9,6 +9,7 @@ return [
         $seeds[] = ['group'=> 'books', 'uri_pattern'=> '/books/by-link|GET',];
         $seeds[] = ['group'=> 'books', 'uri_pattern'=> '/books/by-link|POST',];
         $seeds[] = ['group'=> 'books', 'uri_pattern'=> '/books/search|POST',];
+        $seeds[] = ['group'=> 'books', 'uri_pattern'=> '/books/report|POST',];
 
         $seeds[] = ['group'=> 'download', 'uri_pattern'=> '/download/:uid|GET',];
 
