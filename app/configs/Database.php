2285117da33757058a9d0e7c2f82ca34285a6a46
borvelt <borvelt@borvelts-MacBook-Pro.local>
2016-04-13 03:12:42 +0430

    test

--- app/configs/Database.php
+++ app/configs/Database.php
@@ -3,9 +3,9 @@ return array
     (
         'driver' => 'mysql',
         'host' => '127.0.0.1',
-        'database' => 'science_access',
-        'username' => 'root',
-        'password' => 'root',
+        'database' => 'admin_science',
+        'username' => 'admin_science',
+        'password' => '5C6ZKLFo',
         'prefix'    => '',
         'charset'   => "utf8",
         'collation' => "utf8_unicode_ci"
