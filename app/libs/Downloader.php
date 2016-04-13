9c13dcd4ada6f0ae385db5894034101e645716de
borvelt <borvelt@borvelts-MacBook-Pro.local>
2016-04-14 01:01:12 +0430

    test

--- app/libs/Downloader.php
+++ app/libs/Downloader.php
@@ -318,7 +318,7 @@ class Downloader {
 
     private function setProxy () {
         //download http://hideme.ru/api/proxylist.php?out=plain&code=973164094&uptime=350&ports=8080&anon=4
-        $proxy = file_get_contents("http://hideme.ru/api/proxylist.php?out=plain&code=973164094&uptime=350&ports=8080");
+        $proxy = file_get_contents("http://hideme.ru/api/proxylist.php?out=plain&code=973164094&uptime=100&ports=8080&type=s&anon=23");
         //split it to array load randomly
         $proxys = explode("\n", $proxy);
         $random = rand(0,count($proxy));
