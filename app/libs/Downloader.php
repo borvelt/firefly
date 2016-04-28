1035178e54dd4220283f5f261517911c6e03a002
borvelt <borvelt@borvelts-MacBook-Pro.local>
2016-04-28 16:07:35 +0430

    some changes

--- app/libs/Downloader.php
+++ app/libs/Downloader.php
@@ -190,7 +190,7 @@ class Downloader {
         if (!file_exists(Config::app('webDirectory') . 'download/')) {
             mkdir(Config::app('webDirectory') . 'download/' , 0755, true);
         }
-        $filename = str_replace($this->skip, $this->replace, $filename);
+        // $filename = str_replace($this->skip, $this->replace, $filename);
         if (file_exists(Config::app('webDirectory') . 'download/' . $filename)) {
             $path = Config::app('webDirectory') . 'download/' . $filename;
             if (filesize($path) > 2500) {
@@ -258,9 +258,7 @@ class Downloader {
         $pdf = Config::app('webDirectory').'download/' . basename($file);
         $filename = Config::app('webDirectory').'download/' . basename($file).'.zip';
         if(!file_exists($filename)) {
-            // system("zip --junk-paths -P www.motarjeminiran.com " . str_replace($this->skip, $this->replace, $filename) . " ". str_replace($this->skip, $this->replace, $pdf));
-            // exit("zip --junk-paths -P www.motarjeminiran.com " . $filename . " ". $pdf);
-            system("zip --junk-paths -P www.motarjeminiran.com " . $filename . " ". $pdf);
+            system("zip --junk-paths -P www.motarjeminiran.com " . str_replace($this->skip, $this->replace, $filename) . " ". str_replace($this->skip, $this->replace, $pdf));
             ob_clean();
         }
         return ['filename' => $filename, 'url' => $this->url];
