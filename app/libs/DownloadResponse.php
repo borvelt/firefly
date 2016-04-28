8f66700eb31e214de53b945d2b02f4e480e0dee7
borvelt <borvelt@borvelts-MacBook-Pro.local>
2016-04-28 15:13:58 +0430

    some changes

--- app/libs/DownloadResponse.php
+++ app/libs/DownloadResponse.php
@@ -7,7 +7,6 @@ class DownloadResponse
         ob_clean();
         $skip = [" ", "-", ",", "&", "*", "(", ")", "#", "@", "!", "~", "=", "+", "^", "%", "$", "/", "\\", "'", "\"","."];
         $replace = ["\ ", "\-", "\,", "\&", "\*", "\(", "\)", "\#", "\@", "\!", "\~", "\=", "\+", "\^", "\%", "\$", "\/", "\\", "\'",'\"', "\."];
-        exit("Content-Disposition: attachment; filename=" . str_replace($skip, $replace, basename($path)));
         header("Content-Type: application/zip");
         header("Content-Disposition: attachment; filename=" . str_replace($skip, $replace, basename($path)));
         header("Content-Length: " . filesize($path));
