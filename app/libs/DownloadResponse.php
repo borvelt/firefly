66839f3b7358e66eb217f7d159c821bab9f08c78
borvelt <borvelt@borvelts-MacBook-Pro.local>
2016-04-29 01:32:40 +0430

    some changes

--- app/libs/DownloadResponse.php
+++ app/libs/DownloadResponse.php
@@ -4,10 +4,8 @@ class DownloadResponse
 {
     public static function responseAsImage($path) {
         ob_clean();
-        $skip = [" ", "-", ",", "&", "*", "(", ")", "#", "@", "!", "~", "=", "+", "^", "%", "$", "/", "\\", "'", "\"","."];
-        $replace = ["\ ", "\-", "\,", "\&", "\*", "\(", "\)", "\#", "\@", "\!", "\~", "\=", "\+", "\^", "\%", "\$", "\/", "\\", "\'",'\"', "\."];
         header("Content-Type: application/zip");
-        header("Content-Disposition: attachment; filename=" . str_replace($skip, $replace, basename($path)));
+        header('Content-Disposition: attachment; filename="' . addslashes(basename($path)) . '"');
         header("Content-Length: " . filesize($path));
         readfile($path);
         exit;
